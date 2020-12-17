383c291f9c9f76636eebd37a24443e87ccfd72ad
luozhouyang <zhouyang.luo@gmail.com>
2020-12-17 20:10:22 +0800

    Add setup.py

--- autophrasex/__init__.py
+++ autophrasex/__init__.py
@@ -1,5 +1,15 @@
+import logging
+
 from .autophrase import AutoPhrase
 from .callbacks import (AbstractCallback, EntropyCallback, IDFCallback,
                         NgramsCallback)
 from .strategy import AbstractStrategy, Strategy
 from .tokenizer import BaiduLacTokenizer, JiebaTokenizer
+
+__name__ = 'autophrasex'
+__version__ = '0.1.0'
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)7s %(filename)20s %(lineno)4d] %(message)s",
+    level=logging.INFO
+)
