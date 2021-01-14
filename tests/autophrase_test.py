89772bd902c8ed70a2173c27c479d8e40903aad0
luozhouyang <zhouyang.luo@gmail.com>
2021-01-14 13:22:23 +0800

    Update test

--- tests/autophrase_test.py
+++ tests/autophrase_test.py
@@ -10,24 +10,42 @@ from autophrasex.tokenizer import BaiduLacTokenizer, JiebaTokenizer
 
 class AutoPhraseTest(unittest.TestCase):
 
-    def test_autophrase(self):
+    def test_autophrase_small(self):
         tokenizer = BaiduLacTokenizer()
         # tokenizer = JiebaTokenizer()
         strategy = Strategy(
             tokenizer=tokenizer,
             N=4,
             epsilon=1e-6,
-            threshold=0.45,
-            threshold_schedule_factor=1.0)
+            prob_threshold=0.25,
+            prob_threshold_schedule_factor=1.0)
+        ap = AutoPhrase()
+        predictions = ap.mine(
+            input_doc_files=['data/sogou_news_tensite_content.100.txt'],
+            quality_phrase_files='data/wiki_quality.txt',
+            strategy=strategy,
+            N=4,
+            epochs=10,
+        )
+        for p in predictions:
+            print(p)
 
-        # files = [os.path.join('data/clean_text', f) for f in os.listdir('data/clean_text')]
+    def test_autophrase_large(self):
+        # tokenizer = BaiduLacTokenizer()
+        tokenizer = JiebaTokenizer()
+        strategy = Strategy(
+            tokenizer=tokenizer,
+            N=4,
+            epsilon=1e-6,
+            prob_threshold=0.45,
+            prob_threshold_schedule_factor=1.05,
+            phrase_min_freq=2,
+            phrase_max_count=1000)
+
         ap = AutoPhrase()
         predictions = ap.mine(
             input_doc_files=['data/sogou_news_tensite_content.10000.txt'],
-            # input_doc_files=[os.path.join('data/medical', f) for f in os.listdir('data/medical')],
-            # input_doc_files=['data/medical/data_2.txt'],
-            # input_doc_files=files,
-            quality_phrase_files='data/wiki_quality.txt',
+            quality_phrase_files=['data/wiki_quality.txt', 'data/quality_pred.txt'],
             strategy=strategy,
             N=4,
             epochs=10,
